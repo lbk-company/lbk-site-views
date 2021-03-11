ab85b8881e691b52d66b9b41e9d27c6b647c63bd
Briki <vuongbx17@gmail.com>
2021-03-11 10:41:44 +0700

    fix

--- includes/count.php
+++ includes/count.php
@@ -5,26 +5,29 @@ if ( !defined('ABSPATH') ) die();
 if ( !isset( $_COOKIE['lbk_viewed'] ) ) {
     setcookie( 'lbk_viewed', 'lbk', time() + (5) , '/');
 
-    add_action( 'wp_footer', 'count_view' );
-    function count_view() {
-        global $wpdb;
-        $tablename = $wpdb->prefix."countview";
-        $date = current_time('Y-m-d');
-
-        $sql = "SELECT SUM(count_view) as count_view FROM $tablename WHERE thoi_gian = '$date'";
-        $rs = $wpdb->get_results(
-            $wpdb->prepare($sql)
-        );
-        if ( $rs[0]->count_view == NULL ) {
-            $count = 1;
-            $query = "INSERT INTO $tablename(thoi_gian, count_view) VALUES ('$date',$count)";
-            $wpdb->query($wpdb->prepare($query));
+    if ( !function_exists('lbk_count_view') ) {
+        function lbk_count_view() {
+            global $wpdb;
+            $tablename = $wpdb->prefix."countview";
+            $date = current_time('Y-m-d');
+    
+            $sql = "SELECT SUM(count_view) as count_view FROM $tablename WHERE thoi_gian = '$date'";
+            $rs = $wpdb->get_results(
+                $wpdb->prepare($sql)
+            );
+            if ( $rs[0]->count_view == NULL ) {
+                $count = 1;
+                $query = "INSERT INTO $tablename(thoi_gian, count_view) VALUES ('$date',$count)";
+                $wpdb->query($wpdb->prepare($query));
+            }
+            else {
+                $count = $rs[0]->count_view;
+                $count++;
+                $query = "UPDATE $tablename SET count_view=$count WHERE thoi_gian='$date'";
+                $wpdb->query($wpdb->prepare($query));
+            }
         }
-        else {
-            $count = $rs[0]->count_view;
-            $count++;
-            $query = "UPDATE $tablename SET count_view=$count WHERE thoi_gian='$date'";
-            $wpdb->query($wpdb->prepare($query));
-        }
+        add_action( 'wp_footer', 'count_view' );
     }
+    
 }
--- includes/shortcode.php
+++ includes/shortcode.php
@@ -2,56 +2,60 @@
 // Die if accessed directly
 if ( !defined('ABSPATH') ) die();
 
-add_shortcode( 'lbk_total_views', 'total_view' );
-add_shortcode( 'lbk_today_views', 'today_view' );
+add_shortcode( 'lbk_total_views', 'lbk_total_views' );
+add_shortcode( 'lbk_today_views', 'lbk_today_views' );
 
-function total_view() {
-    global $wpdb;
-    $tablename = $wpdb->prefix."countview";
-
-    $sql = "SELECT SUM(count_view) as count_view FROM $tablename";
-    $rs = $wpdb->get_results(
-        $wpdb->prepare($sql)
-    );
-    $views = $rs[0]->count_view;
-    $view = array();
+if ( !function_exists('lbk_total_views') ) {
+    function lbk_total_views() {
+        global $wpdb;
+        $tablename = $wpdb->prefix."countview";
     
-    for ($i = 0; $i<strlen($views); $i++) {
-        $view[$i] = $views[$i];
+        $sql = "SELECT SUM(count_view) as count_view FROM $tablename";
+        $rs = $wpdb->get_results(
+            $wpdb->prepare($sql)
+        );
+        $views = $rs[0]->count_view;
+        $view = array();
+        
+        for ($i = 0; $i<strlen($views); $i++) {
+            $view[$i] = $views[$i];
+        }
+        ob_start();
+        ?>
+        <div class="lbk">
+            <?php foreach ($view as $count) { ?>
+            <span class="count-view"><?= $count ?></span>
+            <?php } ?>
+        </div>
+        
+        <?php return ob_get_clean();
     }
-    ob_start();
-    ?>
-    <div class="lbk">
-        <?php foreach ($view as $count) { ?>
-        <span class="count-view"><?= $count ?></span>
-        <?php } ?>
-    </div>
-    
-    <?php return ob_get_clean();
 }
 
-function today_view() {
-    global $wpdb;
-    $tablename = $wpdb->prefix."countview";
-    $date = current_time('Y-m-d');
-
-    $sql = "SELECT SUM(count_view) as count_view FROM $tablename WHERE thoi_gian = '$date'";
-    $rs = $wpdb->get_results(
-        $wpdb->prepare($sql)
-    );
-    $views = $rs[0]->count_view;
-    $view = array();
+if ( !function_exists('lbk_today_views') ) {
+    function lbk_today_views() {
+        global $wpdb;
+        $tablename = $wpdb->prefix."countview";
+        $date = current_time('Y-m-d');
     
-    for ($i = 0; $i<strlen($views); $i++) {
-        $view[$i] = $views[$i];
+        $sql = "SELECT SUM(count_view) as count_view FROM $tablename WHERE thoi_gian = '$date'";
+        $rs = $wpdb->get_results(
+            $wpdb->prepare($sql)
+        );
+        $views = $rs[0]->count_view;
+        $view = array();
+        
+        for ($i = 0; $i<strlen($views); $i++) {
+            $view[$i] = $views[$i];
+        }
+        ob_start();
+        ?>
+        <div class="lbk">
+            <?php foreach ($view as $count) { ?>
+            <span class="count-view"><?= $count ?></span>
+            <?php } ?>
+        </div>
+        
+        <?php return ob_get_clean();
     }
-    ob_start();
-    ?>
-    <div class="lbk">
-        <?php foreach ($view as $count) { ?>
-        <span class="count-view"><?= $count ?></span>
-        <?php } ?>
-    </div>
-    
-    <?php return ob_get_clean();
 }
